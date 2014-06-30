4dade40be9bea5a22f121d76c2b8db6ee47db056
getninjaN <getninjan@gmail.com>
2014-06-30 13:58:41 +0200

    Multiple tracker support
    Added support for multiple trackers in GoogleUniversalHandler.

--- src/googleHandler.js
+++ src/googleHandler.js
@@ -14,14 +14,27 @@
         return service;
     }).factory('AngularyticsGoogleUniversalHandler', function () {
         var service = {};
+        var trackers = []
 
         service.trackPageView = function (url) {
-            ga('set', 'page', url);
-            ga('send', 'pageview', url);
+            if(ga) {
+                trackers = ga.getAll();
+
+                angular.forEach(trackers, function(tracker) {
+                    tracker.set('page', url);
+                    tracker.send('pageview', url);
+                });
+            }
         };
 
         service.trackEvent = function (category, action, opt_label, opt_value, opt_noninteraction) {
-            ga('send', 'event', category, action, opt_label, opt_value, {'nonInteraction': opt_noninteraction});
+            if(ga) {
+                trackers = ga.getAll();
+
+                angular.forEach(trackers, function(tracker) {
+                    tracker.send('event', category, action, opt_label, opt_value, {'nonInteraction': opt_noninteraction});
+                });
+            }
         };
 
         return service;
