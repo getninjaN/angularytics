b4d4b1d74728bfec9e239d37b9f1951d98e5c169
getninjaN <getninjan@gmail.com>
2014-07-10 11:01:28 +0200

    Minor adjustment.

--- src/googleHandler.js
+++ src/googleHandler.js
@@ -14,27 +14,19 @@
         return service;
     }).factory('AngularyticsGoogleUniversalHandler', function () {
         var service = {};
-        var trackers = []
+        var trackers = ga.getAll() || []
 
         service.trackPageView = function (url) {
-            if(ga) {
-                trackers = ga.getAll();
-
-                angular.forEach(trackers, function(tracker) {
-                    tracker.set('page', url);
-                    tracker.send('pageview', url);
-                });
-            }
+            angular.forEach(trackers, function(tracker) {
+                tracker.set('page', url);
+                tracker.send('pageview', url);
+            });
         };
 
         service.trackEvent = function (category, action, opt_label, opt_value, opt_noninteraction) {
-            if(ga) {
-                trackers = ga.getAll();
-
-                angular.forEach(trackers, function(tracker) {
-                    tracker.send('event', category, action, opt_label, opt_value, {'nonInteraction': opt_noninteraction});
-                });
-            }
+            angular.forEach(trackers, function(tracker) {
+                tracker.send('event', category, action, opt_label, opt_value, {'nonInteraction': opt_noninteraction});
+            });
         };
 
         return service;
